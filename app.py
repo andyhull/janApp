15a207dd67c89786f2ad14bdc86b15b3b4f58020
Andy Hull <andyhull3@gmail.com>
2013-01-14 20:45:45 -0800

    adding twillio in new run.py file

--- app.py
+++ app.py
@@ -1,15 +1,13 @@
-from flask import Flask, request, redirect
-import twilio.twiml
- 
+import os
+from flask import Flask
+
 app = Flask(__name__)
- 
-@app.route("/", methods=['GET', 'POST'])
-def hello_monkey():
-    """Respond to incoming calls with a simple text message."""
- 
-    resp = twilio.twiml.Response()
-    resp.sms("Hello, Mobile Monkey")
-    return str(resp)
- 
-if __name__ == "__main__":
-    app.run(debug=True)
+
+@app.route('/')
+def hello():
+    return 'Hello World!'
+
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
