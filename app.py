30db20b78946be0f79e3e42a1626b49aebf88020
Andy Hull <andyhull3@gmail.com>
2013-01-14 20:32:04 -0800

    first twillio app

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
-import os
-from flask import Flask
-
+import twilio.twiml, os
+from flask import Flask, request, redirect
+ 
 app = Flask(__name__)
-
-@app.route('/')
-def hello():
-    return 'Hello World!'
-
-if __name__ == '__main__':
-    # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+ 
+@app.route("/", methods=['GET', 'POST'])
+def hello_monkey():
+    """Respond to incoming calls with a simple text message."""
+ 
+    resp = twilio.twiml.Response()
+    resp.sms("Hello, Mobile Monkey")
+    return str(resp)
+ 
+if __name__ == "__main__":
+    app.run(debug=True)
