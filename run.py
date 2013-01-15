1fb284ca7d1edbe87f6a8b94827806d99f7ed0e9
Andy Hull <andyhull3@gmail.com>
2013-01-14 20:55:55 -0800

    adding response tutuoril to run.py

--- run.py
+++ run.py
@@ -1,17 +1,32 @@
 from flask import Flask, request, redirect
-import twilio.twiml, os
+import twilio.twiml
  
 app = Flask(__name__)
  
+# Try adding your own number to this list!
+callers = {
+    "+19196361635": "Andy",
+    "+14158675310": "Boots",
+    "+14158675311": "Virgil",
+}
+ 
 @app.route("/", methods=['GET', 'POST'])
 def hello_monkey():
-    """Respond to incoming calls with a simple text message."""
+    """Respond and greet the caller by name."""
+ 
+    from_number = request.values.get('From', None)
+    if from_number in callers:
+        message = callers[from_number] + ", thanks for the message!"
+    else:
+        message = "Monkey, thanks for the message!"
  
     resp = twilio.twiml.Response()
-    resp.sms("Hello, Mobile Monkey")
-    return str(resp)
+    resp.sms(message)
  
+    return str(resp)
+
 if __name__ == "__main__":
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
+    app.run(debug=True)
