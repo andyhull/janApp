3c0344c60c4bc3e81a24b80bfb00442cf3cceef1
Jake Solomon <jacob@codeforamerica.org>
2013-01-18 14:19:27 -0800

    trying to get it to run locally

--- smsbuddy/app.py
+++ smsbuddy/app.py
@@ -3,8 +3,6 @@ import os
 from database import db_session, init_db
 from flask import Flask
 import views
-from smsbuddy import app
-app.run(debug=True)
 
 DEBUG = True
 SECRET_KEY = 'development key'
@@ -12,6 +10,7 @@ SECRET_KEY = 'development key'
 app = Flask(__name__)
 app.config.from_object(__name__)
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+app.run(debug=True)
 init_db()
 
 if __name__ == '__main__':
