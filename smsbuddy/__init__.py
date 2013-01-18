dbfde9a4fb71aa5aee14009fbb6d48fbd101b3ee
Andy Hull <andyhull3@gmail.com>
2013-01-18 13:38:13 -0800

    getting running on heroku

--- smsbuddy/__init__.py
+++ smsbuddy/__init__.py
@@ -11,8 +11,3 @@ app.config.from_object(__name__)
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 init_db()
 from smsbuddy import views
-
-if __name__ == "__main__":
-    # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
