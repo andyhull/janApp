c54f2110fe274d99abad3816d64c344d8ddb4ce1
Andy Hull <andyhull3@gmail.com>
2013-01-18 14:03:51 -0800

    heroku config

--- smsbuddy/app.py
+++ smsbuddy/app.py
@@ -2,6 +2,9 @@
 import os
 from database import db_session, init_db
 from flask import Flask
+import views
+from smsbuddy import app
+app.run(debug=True)
 
 DEBUG = True
 SECRET_KEY = 'development key'
@@ -10,10 +13,3 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 init_db()
-from smsbuddy import views
-
-
-if __name__ == '__main__':
-    # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
