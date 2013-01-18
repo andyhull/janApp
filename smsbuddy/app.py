d0b9b038201d5bb6346446ed8938cd1c482ebedd
Andy Hull <andyhull3@gmail.com>
2013-01-18 14:07:38 -0800

    heroku config

--- smsbuddy/app.py
+++ smsbuddy/app.py
@@ -13,3 +13,8 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 init_db()
+
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
