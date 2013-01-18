0dfec01031ffe71d8b7c4c576fe97b1256df36bd
Andy Hull <andyhull3@gmail.com>
2013-01-18 15:48:18 -0800

    fixed add route

--- smsbuddy/app.py
+++ smsbuddy/app.py
@@ -1,21 +1,22 @@
+from flask import Flask, request, session, g, redirect, url_for, abort, \
+     render_template, flash, _app_ctx_stack
 import os
 from database import engine, db_session, init_db
-from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
 from flask.ext.sqlalchemy import SQLAlchemy
 from models import Numbers
+# import logging, sys
+# logging.basicConfig(stream=sys.stderr)
+# import views
+
+DEBUG = True
+SECRET_KEY = 'development key'
 
 app = Flask(__name__)
+app.config.from_object(__name__)
 
 @app.teardown_request
 def shutdown_session(exception=None):
     db_session.remove()
-
-@app.route('/')
-def index():
-    cur = db_session.execute('select phone, buddy from numbers order by id desc')
-    cur = Numbers.query.all()
-
-    return render_template('show_entries.html', entries=cur)
 
 @app.route('/add', methods=['POST'])
 def add_entry():
@@ -33,10 +34,17 @@ def add_entry():
 
     flash('New entry was successfully posted')
     return redirect(url_for('index'))
+@app.route('/')
+def index():
+    cur = db_session.execute('select phone, buddy from numbers order by id desc')
+    cur = Numbers.query.all()
+
+    return render_template('show_entries.html', entries=cur)
+
 
 if __name__ == '__main__':
-	init_db()
 	# Bind to PORT if defined, otherwise default to 5000.
 	port = int(os.environ.get('PORT', 5000))
 	app.run(host='0.0.0.0', port=port)
+	init_db()
 
