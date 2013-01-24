90d26a1773577ab9908e4e121d30cff1ff50cee8
Andy Hull <andyhull3@gmail.com>
2013-01-24 09:46:27 -0800

    fixing heroku

--- smsbuddy/app.py
+++ smsbuddy/app.py
@@ -6,15 +6,9 @@ from flask import Flask, request, session, g, redirect, url_for, abort, render_t
 from flask_sqlalchemy import SQLAlchemy
 from models import Numbers
 from twilio.rest import TwilioRestClient
-from config import account_sid, auth_token, twilio_number
-
+# from config import account_sid, auth_token, twilio_number
 DEBUG = True
 SECRET_KEY = 'development key'
-
-#DB replacement for testing Twilio
-loner = None
-numbers = []
-convos = {}
 
 app = Flask(__name__)
 app.config.from_object(__name__)
@@ -129,50 +123,9 @@ def send_sms(number, body):
     message = client.sms.messages.create(to=number, from_=twilio_number,
                                      body=body)
 
-@app.route('/receiver', methods=['GET', 'POST'])
-def receiver():
-    from_number = request.values.get('From', None)
-    body = request.values.get('Body', None)
-    app.logger.debug('From: %s, Body: %s' % (from_number, body))
-
-    #initiate new number
-    if from_number not in numbers:
-        initiate_number(from_number)
-        return 'Text me: 415.539.3977'
-
-    partner = get_partner(from_number)
-    if partner:
-        send_sms(partner, body)
-    else:
-        send_sms(from_number, "We're still waiting to match you. Sorry about that!")
-    app.logger.debug('From: %s \nPartner: %s \nloner: %s \n convos: %s' % (from_number, partner, loner, convos))
-    return 'Text me: 510-213-6505'
-
-def initiate_number(number):
-    global loner
-    numbers.append(number)
-    if not loner:
-        loner = number
-        send_sms(number, "There aren't any people to match you with quite yet.")
-    else:
-        convos[number] = loner
-        convos[loner] = number
-        body = "You've been matched. Feel free to share your experience."
-        send_sms(number, body)
-        send_sms(loner, body)
-        loner = None
-
-def get_partner(number):
-    if number in convos:
-        return convos[number]
-    return None
-
-def send_sms(number, body):
-    client = TwilioRestClient(account_sid, auth_token)
-    message = client.sms.messages.create(to=number, from_=twilio_number,
-                                     body=body)
 if __name__ == '__main__':
     init_db()
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
+
